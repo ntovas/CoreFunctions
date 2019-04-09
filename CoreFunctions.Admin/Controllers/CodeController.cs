b96bc727bedcdb9e595d00798cf72e1d379688f6
Ntovas <ntovas.gi@gmail.com>
2019-04-09 23:43:14 +0300

    Add Code Completion

---
+++ CoreFunctions.Admin/Controllers/CodeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreFunctions.CodeAnalysis;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFunctions.Admin.Controllers
+{
+    public class CodeController : Controller
+	{
+	    public async Task<IActionResult> GetCompletion(string code)
+	    {
+		    var data = await CodeCompletion.Run(code, code.Length -1);
+
+			return Ok(data);
+	    }
+    }
+}
---
+++ CoreFunctions.CodeAnalysis/CodeCompletion.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Composition.Hosting;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Completion;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.Text;
+
+namespace CoreFunctions.CodeAnalysis
+{
+	public static class CodeCompletion
+	{
+		public static async Task<List<string>> Run(string script, int position)
+		{
+			var assemblies = new[]
+			{
+				Assembly.Load("Microsoft.CodeAnalysis"),
+				Assembly.Load("Microsoft.CodeAnalysis.CSharp"),
+				Assembly.Load("Microsoft.CodeAnalysis.Features"),
+				Assembly.Load("Microsoft.CodeAnalysis.CSharp.Features"),
+			};
+
+			var parts = MefHostServices.DefaultAssemblies.Concat(assemblies)
+				.Distinct()
+				.SelectMany(x => x.GetTypes())
+				.ToArray();
+
+			var ctx = new ContainerConfiguration()
+				.WithParts(parts)
+				.CreateContainer();
+			var host = MefHostServices.Create(ctx);
+			var workspace = new AdhocWorkspace(host);
+
+			var compilationOptions = new CSharpCompilationOptions(
+				OutputKind.DynamicallyLinkedLibrary, usings: new[] { "System",
+					"CoreFunctions", "Microsoft.AspNetCore.Http",
+					"System.Net" });
+			var scriptProjectInfo = ProjectInfo.Create(ProjectId.CreateNewId(),
+					VersionStamp.Create(), "Script", "Script",
+					LanguageNames.CSharp, isSubmission: true)
+				.WithMetadataReferences(new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location) })
+				.WithCompilationOptions(compilationOptions);
+
+			var scriptProject = workspace.AddProject(scriptProjectInfo);
+			var scriptDocumentInfo = DocumentInfo.Create(
+				DocumentId.CreateNewId(scriptProject.Id), "Script",
+				sourceCodeKind: SourceCodeKind.Script,
+				loader: TextLoader.From(
+					TextAndVersion.Create(SourceText.From(script),
+						VersionStamp.Create())));
+			var scriptDocument = workspace.AddDocument(scriptDocumentInfo);
+			var compiler = CompletionService.GetService(scriptDocument);
+			return (await compiler.GetCompletionsAsync(scriptDocument, position))
+				.Items
+				.Select(c=> c.DisplayText).ToList();
+		}
+
+	}
+}
